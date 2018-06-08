f48dd06928d5a84c891cb87ec4cd8b3235961df4
Thanos Diamantidis <thanosdi@live.com>
2018-06-08 11:39:37 +0300

    Add renameKeys and createObjFromArray runctions

--- src/index.js
+++ src/index.js
@@ -1,1 +1,46 @@
-module.exports = {};
+const { curry, reduce, assoc, keys } = require('ramda');
+/**
+ * Creates a new object with the own properties of the provided object, but the
+ * keys renamed according to the keysMap object as `{oldKey: newKey}`.
+ * When some key is not found in the keysMap, then it's passed as-is.
+ *
+ * Keep in mind that in the case of keys conflict is behaviour undefined and
+ * the result may vary between various JS engines!
+ *
+ * @sig {a: b} -> {a: *} -> {b: *}
+ *
+ * Usage:
+ * const input = { firstName: 'Elisia', age: 22, type: 'human' }
+ *
+ * renameKeys({ firstName: 'name', type: 'kind', foo: 'bar' })(input)
+ * //=> { name: 'Elisia', age: 22, kind: 'human' }
+ */
+const renameKeys = curry((keysMap, obj) =>
+	reduce(
+		(acc, key) => assoc(keysMap[key] || key, obj[key], acc),
+		{},
+		keys(obj),
+	),
+);
+
+/**
+ * Creates an object from an array of objects with {key:'key', value:'value'} pairs
+ *
+ *
+ * @sig [{a}] → {k: v}
+ *
+ * Usage:
+ * const input = [{ key: 'Thanos', value: 'di' }, { key: 'Marios', value: 'ant' }]
+ *
+ * createObjFromArray(input)
+ * //=> { Thanos: 'di', Marios: 'ant' }
+ */
+const createObjFromArray = reduce(
+	(prev, { key, value }) => assoc(key, value, prev),
+	{},
+);
+
+module.exports = {
+	renameKeys,
+	createObjFromArray,
+};
--- src/index.test.js
+++ src/index.test.js
@@ -1,6 +1,23 @@
-test('adds 1 + 2 to equal 3', () => {
-  const sum = 1 + 2;
+const Runction = require('.');
 
-  expect(sum).toBe(3)
+test('Runction renameKeys', () => {
+	const input = { firstName: 'Elisia', age: 22, type: 'human' };
+	const renamedObj = Runction.renameKeys({
+		firstName: 'name',
+		type: 'kind',
+		foo: 'bar',
+	})(input);
+
+	expect(renamedObj).toEqual({ name: 'Elisia', age: 22, kind: 'human' });
 });
 
+test('Runction createObjFromArray', () => {
+	const input = [
+		{ key: 'Thanos', value: 'di' },
+		{ key: 'Marios', value: 'ant' },
+	];
+
+	const obj = Runction.createObjFromArray(input);
+
+	expect(obj).toEqual({ Thanos: 'di', Marios: 'ant' });
+});
